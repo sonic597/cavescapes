35f4d6b92ba866254e0bbdf2c9606b5ec8ff4ac8
sonic597 <30410081+sonic597@users.noreply.github.com>
2023-07-24 18:32:00 +0800

    Add files via upload
    added config file to make it more convenient to change game properties

---
+++ Cavescapes/game_config.py
@@ -0,0 +1,8 @@
+# CONFIG FILE TO CHANGE EDITABLE VARIABLES
+grid_size = 5
+
+#AI Variables
+elipson = 0.01 # the rate of exploring vs. exploiting (between 0 (exploits every turn) and 1 (explores every turn))
+unseen_weighting = 2 # the weighting (usually >1) to unseen tiles to encourage (or discourage) new pathfinding
+unk_weighting = 5 # the bonus added to the value for an unseen block type (higher values encourage exploration)
+
